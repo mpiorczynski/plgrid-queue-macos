2843473df971416285ec6575453210a85aad310b
xweinp <xweinp@gmail.com>
2026-08-26 11:18:07 +0200

    preserve workspace indicators on far left when placing extension in left panel box

--- extension.js
+++ extension.js
@@ -13,12 +13,21 @@ export default class PlgridQueueExtension extends Extension {
         this._settings = this.getSettings();
         this._indicator = new Indicator(this._settings, () => this.openPreferences());
 
+        const panelBoxName = this._settings.get_string('panel-box');
+        let initialIndex = this._settings.get_int('panel-index');
+        if (panelBoxName === 'left' && initialIndex <= 0) {
+            initialIndex = 1;
+        }
+
         Main.panel.addToStatusArea(
             this.uuid,
             this._indicator,
-            this._settings.get_int('panel-index'),
-            this._settings.get_string('panel-box')
+            initialIndex,
+            panelBoxName
         );
+
+        // Ensure correct position relative to workspace switcher / activities
+        this._reposition();
 
         this._placementChangedId = this._settings.connect(
             'changed::panel-box',
@@ -33,21 +42,32 @@ export default class PlgridQueueExtension extends Extension {
     _reposition() {
         if (!this._indicator) return;
 
+        const panelBoxName = this._settings.get_string('panel-box');
         const boxes = {
             left: Main.panel._leftBox,
             center: Main.panel._centerBox,
             right: Main.panel._rightBox,
         };
 
-        const box = boxes[this._settings.get_string('panel-box')] ?? Main.panel._rightBox;
+        const box = boxes[panelBoxName] ?? Main.panel._rightBox;
         const container = this._indicator.container;
         const parent = container.get_parent();
         if (parent) {
             parent.remove_child(container);
         }
 
-        const index = Math.min(this._settings.get_int('panel-index'), box.get_n_children());
-        box.insert_child_at_index(container, index);
+        const totalChildren = box.get_n_children();
+        let targetIndex = this._settings.get_int('panel-index');
+
+        if (panelBoxName === 'left') {
+            // Keep workspace indicators / Activities on the far left.
+            // Place indicator after them (at least index 1, or at the end of leftBox).
+            targetIndex = targetIndex <= 0 ? totalChildren : Math.max(1, Math.min(targetIndex, totalChildren));
+        } else {
+            targetIndex = Math.min(targetIndex, totalChildren);
+        }
+
+        box.insert_child_at_index(container, targetIndex);
     }
 
     disable() {
